b10772b2f2114153bf96394a11880f7d9599eb35
follikk@gmail.com <follikk@gmail.com>
2019-05-07 21:17:32 +0300

    added form

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { InterfaceStyle } from '../models/interface';
+import { UserSettings } from '../models/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
   styleUrls: ['./user-settings-form.component.scss']
 })
-export class UserSettingsFormComponent implements OnInit {
 
+export class UserSettingsFormComponent implements OnInit {
+  private originalUserSettings: UserSettings = {
+    name: 'Roman',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'here are some notes...'
+  };
+  private userSettings: UserSettings = { ...this.originalUserSettings };
   private subscriptionTypes: Array<string> = ['Monthly', 'Annual', 'Lifetime'];
   private interfacies: Array<InterfaceStyle> = [
     { id: 'lightInterface', value: 'light', label: 'Light' },
